b3c9333dbf1ae2ce447129e426f3928d66505ff5
Artyom Efimenko <54911543+arefimenko@users.noreply.github.com>
2025-11-21 11:07:42 +0300

    feat: kotlin scope functions let and also as extensions (#1)

--- example/caxapok_example.dart
+++ example/caxapok_example.dart
@@ -1,6 +1,12 @@
 import 'package:caxapok/caxapok.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  // kotlin let extension
+  final mathResult = 12.let((it) => it * 2).let((it) => it + 3);
+  print(mathResult); // prints 27
+
+  // kotlin also extension
+  final numbers = ["one", "two", "three"];
+  // prints "The list elements before adding new one: [one, two, three]"
+  numbers.also((it) => print("The list elements before adding new one: $it")).add("four");
 }
